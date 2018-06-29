78eb315f17ce6a0ef786c2cd37f01eef2d2973f4
kanayannet <kanayannet@gmail.com>
2018-06-29 15:31:31 +0900

    babel was added for es5 browser and npm modules update

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,28 @@ module.exports = {
   output: {
     filename: 'index.js',
     path: path.join(__dirname, './')
+  },
+  module: {
+    rules: [
+      {
+        // 拡張子 .js の場合
+        test: /\.js$/,
+        use: [
+          {
+            // Babel を利用する
+            loader: 'babel-loader',
+            // Babel のオプションを指定する
+            options: {
+              presets: [
+                // env を指定することで、ES2018 を ES5 に変換。
+                // {modules: false}にしないと import 文が Babel によって CommonJS に変換され、
+                // webpack の Tree Shaking 機能が使えない
+                ['env', {'modules': false}]
+              ]
+            }
+          }
+        ]
+      }
+    ]
   }
-};
+}
